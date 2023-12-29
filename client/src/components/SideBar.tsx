7aeac396d4919aab8ba08b8b11dfde45e5c7d54c
John Dunn <johnwdunn20@gmail.com>
2023-12-28 21:01:01 -0800

    began updates to login page

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import close from '../images/close.svg'
 
 const SideBar = (props) => {
 
   // console.log('sideBarOpen: ', props.sideBarOpen);
   return (
     <>
-      {/* <nav id="sideMenu" className={`fixed top-0 right-0 w-80 h-full z-20 bg-gray-800 shadow-lg transform ${props.sideBarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
-        <button onClick={props.closeSideBar} className="absolute top-4 right-4 text-white text-2xl">&times;</button>
-        <h1 className="text-white text-2xl">Shopping List</h1>
-        <h1 className="text-white text-2xl">History</h1>
-      </nav> */}
       <nav id="sideMenu" className={`fixed top-0 left-0 w-64 h-full z-20 bg-secondaryBackgroud border-2 shadow-sm transform ${props.sideBarOpen ? '-translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
-        <button className='absolute top-2 right-1 text-2xl border-2 rounded-sm' onClick={props.closeSideBar}>&times;</button>
-        <h1 className="text-2xl m-2">Shopping List</h1>
+        <div className='flex justify-between content-center border-b-2 border-slate-500'>
+          <h1 className='text-3xl text-primaryBlue font-bold mt-5 m-2'>Navigate</h1>
+          <button className='rounded-sm' onClick={props.closeSideBar}>
+            <img src={close} alt='close' className='h-12 w-12' />
+          </button>
+        </div>
+        <h1 className="text-2xl m-2 mt-5">Shopping List</h1>
         <h1 className="text-2xl m-2">History</h1>
       </nav>
     </>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,28 +1,49 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  email: string
+  password: string
+}
 
 const Login = () => {
   const navigate = useNavigate();
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+    // navigate('/');
+    // navigate needs to happen after verifying authentication
+  }
+
+
   return (
-    <main>
-    <h1>Shopping List</h1>
-    <section className='login-container'>
-      <h3>Login</h3>
-      <form action="#" className='login-form' onSubmit={() => navigate('/')}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" />
-        </div>
-        <input id='submit-button' type="submit" name="" value="Login"/>
-      </form>
-    </section>
+    <main className='h-screen flex flex-col justify-center items-center'>
+      <h1 className="text-primaryBlue text-3xl font-semibold">Shopping List</h1>
+      <h3>Some Subtitle</h3>
+      <section className='flex flex-col justify-center items-center w-4/5 h-1/2 md:h-1/4 md:w-1/2 lg:w-1/4 border-1 border-primaryBlue rounded-xl bg-secondaryBlue'>
+        <h1>Login</h1>
+        <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+          <input className='border' placeholder="johndoe@gmail.com" {...register("email"), { required: true }} />
+
+          <input className='border' placeholder='password' {...register("password", { required: true, minLength: 8 })} />
+
+          {/* errors will return when field validation fails  */}
+          {errors.email && <span>This field is required</span>}
+
+          <input type="button" value="Sign Up" />
+          <input type="submit" value="Log In" />
+        </form>
+      </section>
     </main>
   )
+
 }
 
 export default Login
