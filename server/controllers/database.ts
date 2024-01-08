3c526c81c53cd3a753169eea4149a3b896561445
John Dunn <johnwdunn20@gmail.com>
2024-01-07 20:03:03 -0800

    Basic updates for database controller - need to be integrated with auth, and frontend requests need to be updated

--- server/controllers/database.ts
+++ server/controllers/database.ts
@@ -20,73 +20,43 @@ const databaseController = {
     }
   },
 
-  // add a new item
-  addItem: (req, res, next) => {
+  addItem: async (req, res, next) => {
     const { newItem } = req.body;
-
-    // get id of the category
-    models.Category.findOne({
-      category: res.locals.category,
-    })
-      .then((data) => {
-        if (!data) {
-          return next({
-            log: `Not able to find category. LLM found ${res.locals.category} which does not exist as a standard category`,
-            status: 500,
-            message: {
-              err: `Not able to find category. LLM found ${res.locals.category} which does not exist as a standard category`,
-            },
-          });
-        }
-        console.log(data.id);
-        const categoryId = data.id;
-
-        // use that id to insert into groceries
-        models.Grocery.create({
-          user: "6542a7b6e06d8d00cdf55cb2", // **** hard-coding for now, will update it later
-          itemName: newItem,
-          category: categoryId,
-        }).then((data) => {
-          console.log("Inserted: ", data);
-          next();
-        });
-      })
-      .catch((err) =>
-        next({
-          log: `Express error handler caught error in databaseController.getGroceries. Error: ${err}`,
-          status: 500,
-          message: { err },
-        })
-      );
+    try {
+      // get the category and then push a new item into that arr
+    } catch (err) {
+      return next({
+        log: `Express error handler caught error in databaseController.addItem. Error: ${err}`,
+        status: 500,
+        message: { err },
+      });
+    }
   },
 
   deleteItem: async (req, res, next) => {
-    console.log("invoking deleteItem controller");
-    const id = req.params.id;
-    console.log(id);
-    models.Grocery.findByIdAndDelete(id)
-      .exec()
-      .then((data) => {
-        console.log("Deleted: ", data);
-        res.locals.deletedItem = data;
-        return next();
-      })
-      .catch((err) =>
-        next({
-          log: `Express error handler caught error in databaseController.deleteItem. Error: ${err}`,
-          status: 500,
-          message: { err },
-        })
-      );
+    try {
+      const id = req.params.id;
+      const data = await models.Item.findByIdAndDelete(id).exec();
+      console.log("Deleted: ", data);
+      res.locals.deletedItem = data;
+      return next();
+    } catch (err) {
+      return next({
+        log: `Express error handler caught error in databaseController.deleteItem. Error: ${err}`,
+        status: 500,
+        message: { err },
+      });
+    }
   },
 
   toggleCheck: async (req, res, next) => {
     try {
       const id = req.params.id;
       const { checked } = req.body;
-      const data = await models.Grocery.findByIdAndUpdate(id, {
+      const data = await models.Item.findByIdAndUpdate(id, {
         checked: checked,
       }).exec();
+
       console.log("Updated", data);
       res.locals.updatedItem = data;
       return next();
