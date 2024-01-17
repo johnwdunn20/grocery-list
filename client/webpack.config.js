afed11f58f45f5df2632b006fe14437cbaa77d8b
John Dunn <johnwdunn20@gmail.com>
2024-01-16 20:05:45 -0800

    added favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -79,7 +80,11 @@ const config = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
-    // new Dotenv(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/favicon.ico', to: 'favicon.ico' }
+      ]
+    }),
   ],
   resolve: {
     // Enable importing JS / JSX files without specifying their extension in react components
